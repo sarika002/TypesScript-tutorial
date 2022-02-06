05473499b5a83d76264b68d3d2831aad2118077c
sarika002 <98938893+sarika002@users.noreply.github.com>
2022-02-06 18:41:23 +0530

    Add files via upload

--- JavaScript file/car.js
+++ JavaScript file/car.js
@@ -1,1 +1,13 @@
-
+var Car = /** @class */ (function () {
+    function Car(model, doors, isElectric) {
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+    }
+    Car.prototype.displayMake = function () {
+        console.log("This car is ".concat(this.model));
+    };
+    return Car;
+}());
+var car1 = new Car('Prius', 4, true);
+car1.displayMake(); // This car is Prius
